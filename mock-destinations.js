863543a9ab65b474723db6934ebc1bd76194af07
Navfront <b3rt@yandex.ru>
2022-06-12 18:45:32 +0300

    init

---
+++ mock-destinations.js
@@ -0,0 +1,56 @@
+const EVENT_DESTINATIONS = [
+  { name: 'Chamonix',
+    basePrice: 1300,
+    description:
+      'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building',
+      },
+      {
+        src: 'img/photos/5.jpg',
+        description: 'Random image',
+      },
+    ],
+  },
+  { name: 'Geneva',
+    basePrice: 999,
+    description: 'Geneva is beautiful.',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Geneva looks good',
+      },
+      {
+        src: 'img/photos/1.jpg',
+        description: 'Random image',
+      },
+      {
+        src: 'img/photos/2.jpg',
+        description: 'Random image',
+      },
+    ],
+  },
+  { name: 'Amsterdam',
+    basePrice: 1500,
+    description: 'Amsterdam is nice.',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200',
+        description: 'Geneva looks good',
+      },
+      {
+        src: 'img/photos/3.jpg',
+        description: 'Random image',
+      },
+      {
+        src: 'img/photos/4.jpg',
+        description: 'Random image',
+      },
+    ],
+  },
+];
+
+
+module.exports = EVENT_DESTINATIONS;
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cors = require('cors');
+const ServerModel = require('./model.js')
+
+const serverModel = new ServerModel();
+
+//искуственная задержка
+const TIMEOUT = 1000;
+
+const whitelist = ["http://localhost:8080"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+
+const app = express();
+app.use(express.json("application/json"));
+app.use(cors(corsOptions));
+
+const port = 3001;
+const log = (req, text='') => {
+    console.log('method:', req?.method,'from:', req?.headers?.host,  req?.ip, text, req?.body?.id || '', req?.body?.destination?.name || '');
+}
+
+//all points
+app.get('/api/points', (req, res) => {
+  log(req, 'points')
+  setTimeout(()=>{res.json({message: serverModel.getPoints()})}, TIMEOUT)
+});
+
+//all events
+app.get('/api/events', (req, res) => {
+  log(req, 'events')
+  setTimeout(()=>{res.json({message: serverModel.getEvents()})}, TIMEOUT)
+});
+
+//all destinations
+app.get('/api/destinations', (req, res) => {
+  log(req, 'destinations')
+  setTimeout(()=>{res.json({message: serverModel.getDestinations()})}, TIMEOUT)
+});
+
+//get point by id
+app.post('/api/point', (req, res) => {
+  log(req, 'point by id')
+  setTimeout(()=>{res.json({message: serverModel.getPointById(req.body.id)})}, TIMEOUT)
+});
+
+//create new point by data
+app.post('/api/create', (req, res) => {
+  log(req, 'created: ')
+  setTimeout(() => {res.json({message: serverModel.createPoint(req.body)})}, TIMEOUT)
+
+})
+
+//update point by data
+app.post('/api/update', (req, res) => {
+  log(req, 'update')
+  setTimeout(() => {res.json({message: serverModel.updatePoint(req.body)})}, TIMEOUT)
+})
+
+//delete point by id
+app.post('/api/delete', (req, res) => {
+  log(req, 'delete')
+  setTimeout(() => {res.json({message: serverModel.deletePoint(req.body.id)})}, TIMEOUT)
+})
+
+//sync points
+app.post('/api/sync', (req, res) => {
+  log(req, 'sync')
+  setTimeout(() => {res.json({message: serverModel.syncPoints(req.body)})}, TIMEOUT)
+})
+
+app.post('*', (req, res) => {
+  log(req)
+  //WIP
+  res.json({
+    message: 'post ok',
+    body: req.body})
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+// "node server/server.js" to run this
